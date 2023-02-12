af97bf56c182a6641e34034102904950f1d4976a
Vithor <vithorpileco@gmail.com>
2023-02-12 17:25:09 -0300

    Implement OnSaveMusicBtn and OnGenerateMusicBtn

--- src/main/java/com/ufrgs/MainWindow.java
+++ src/main/java/com/ufrgs/MainWindow.java
@@ -1,6 +1,7 @@
 package com.ufrgs;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 
 // Ao criada deve inicializar a biblioteca de interface gráfica e criar e exibir a tela do programa, também deve responder a inputs do usuário;
@@ -9,6 +10,7 @@ public class MainWindow {
     JButton GenerateMusicBtn;
     JButton SaveMusicBtn;
     JButton PauseMusicBtn;
+    Music music;
     JTextArea inputText;
     // TODO WidgetPlayer widgetPlayer;
     JPanel instructionTable;
@@ -26,9 +28,18 @@ public class MainWindow {
             frame.setVisible(true);
     }
     // Callback que ocorre quando o usuário clica no botão para gerar a música, deve pegar o texto atual e passa-lo para a musicFactory;
-    void OnGenerateMusicBtn(){}
+    void OnGenerateMusicBtn(){
+        music = MusicFactory.createMusic(inputText.getText());
+    }
     // Callback  que ocorre quando o usuário clica no botão para salvar a música, deve chamar o método save da classe Music;
-    void OnSaveMusicBtn(){}
+    void OnSaveMusicBtn(){
+        JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fc.setDialogTitle("Select folder to save music");
+        int result = fc.showDialog(null, "Select");
+        if (result == JFileChooser.APPROVE_OPTION)
+             music.saveMusic(fc.getSelectedFile().getAbsolutePath());
+    }
     // Callback que ocorre quando o usuário clica no botão para pausar/start a música, deve tocar ou pausar a música dependendo do estado anterior, chamando o método correspondente da classe Music;
     void OnPauseBtn(){}
     // Seta o tempo da música na barra de progressão;
