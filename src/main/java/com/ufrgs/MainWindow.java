b3b28814036f61860282333a9fdf0d3ee82e74f3
Vithor <vithorpileco@gmail.com>
2023-02-14 23:12:03 -0300

    Add customized input file button

--- src/main/java/com/ufrgs/MainWindow.java
+++ src/main/java/com/ufrgs/MainWindow.java
@@ -3,6 +3,8 @@ package com.ufrgs;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 // Ao criada deve inicializar a biblioteca de interface gráfica e criar e exibir a tela do programa, também deve responder a inputs do usuário;
 public class MainWindow {
@@ -17,15 +19,38 @@ public class MainWindow {
     private JPanel mainPanel;
     private JButton generateMusicButton;
     private JButton saveMusicButton;
+    private JLabel labelInputFIle;
 
     public MainWindow(){
-            JFrame frame = new JFrame();
-            Toolkit tk=Toolkit.getDefaultToolkit();
-            Dimension screenSize = tk.getScreenSize();
-            frame.setSize(screenSize.width,screenSize.height);
-            frame.add(mainPanel);
-            frame.setTitle("Song Parser");
-            frame.setVisible(true);
+        ImageIcon x = new ImageIcon("/home/vithor/Desktop/musicCreator/icons/document.png");
+        Image img = x.getImage();
+        Image newImg = img.getScaledInstance(50,50, Image.SCALE_SMOOTH);
+        Image hoveredImg = img.getScaledInstance(60,60, Image.SCALE_SMOOTH);
+        labelInputFIle.setIcon(new ImageIcon(newImg));
+        labelInputFIle.addMouseListener(new MouseAdapter() {
+                                            @Override
+                                            public void mouseClicked(MouseEvent mouseEvent) {
+
+                                            }
+
+                                            @Override
+                                            public void mouseEntered(MouseEvent mouseEvent) {
+                                                labelInputFIle.setIcon(new ImageIcon(hoveredImg));
+                                            }
+
+                                            @Override
+                                            public void mouseExited(MouseEvent mouseEvent) {
+                                                labelInputFIle.setIcon(new ImageIcon(newImg));
+                                            }
+                                        }
+        );
+        JFrame frame = new JFrame();
+        Toolkit tk=Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+        frame.setSize(screenSize.width,screenSize.height);
+        frame.add(mainPanel);
+        frame.setTitle("Song Parser");
+        frame.setVisible(true);
     }
     // Callback que ocorre quando o usuário clica no botão para gerar a música, deve pegar o texto atual e passa-lo para a musicFactory;
     void OnGenerateMusicBtn(){
